248ad1586f10a63f843132073e3d4102c04fb334
Andherson <86683483+Andherson92@users.noreply.github.com>
2021-08-29 01:00:42 -0300

    #1 Commit

---
+++ shopping-cart/src/main/java/digitalinnovation/one/experts/shoppingcart/model/Cart.java
@@ -0,0 +1,24 @@
+package digitalinnovation.one.experts.shoppingcart.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("cart")
+public class Cart {
+
+    @Id
+    private Integer id;
+    private List<Item> items;
+
+}
